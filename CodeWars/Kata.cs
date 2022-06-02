1737fc60990840c2a75d3996d16c71b46c9b985c
Sylvain Bie <biesylvain@gmail.com>
2022-06-02 23:45:40 +0200

    easy: gravity flip

---
+++ CodeWars/Kata.cs
@@ -0,0 +1,22 @@
+﻿namespace CodeWars
+{
+    public static class Kata
+    {
+      
+        public static int[] Flip(char dir, int[] arr)
+        {
+
+
+            List<int> numbers = new List<int>(arr);
+            if (dir == 'R')
+            {
+                arr = numbers.OrderBy(x => x).ToArray();
+            }
+            else
+            {
+                arr = numbers.OrderByDescending(x => x).ToArray();
+            }
+            return arr;
+        }
+    }
+}
---
+++ Test/FilpUnitTest.cs
@@ -0,0 +1,63 @@
+using CodeWars;
+using NUnit.Framework;
+
+namespace Test
+{
+    public class FilpUnitTest
+    {
+
+        [Test]
+        public void Test_nothing_to_do()
+        {
+
+            Assert.AreEqual(new int[] { 0, 0, 0, 1 }, Kata.Flip('R', new int[] { 0, 0, 0, 1 }));
+    
+        }
+
+        [Test]
+        public void Test_shift_right()
+        {
+            Assert.AreEqual(new int[] { 0, 1 }, Kata.Flip('R', new int[] { 1, 0 }));
+
+            Assert.AreEqual(new int[] { 0, 0, 0, 1 }, Kata.Flip('R', new int[] { 0, 0, 1, 0 }));
+            Assert.AreEqual(new int[] { 0, 0, 0, 1 }, Kata.Flip('R', new int[] { 0, 1, 0, 0 }));
+            Assert.AreEqual(new int[] { 0 ,0 ,0, 1 }, Kata.Flip('R', new int[] { 1, 0, 0, 0 }));
+
+
+        }
+
+        [Test]
+        public void Test_shift_right2()
+        {
+            Assert.AreEqual(new int[] { 0 ,0, 1, 2 }, Kata.Flip('R', new int[] { 0, 0, 2, 1 }));
+            Assert.AreEqual(new int[] { 0, 0, 1, 2 }, Kata.Flip('R', new int[] { 0, 2, 1, 0 }));
+            Assert.AreEqual(new int[] { 0, 0, 1, 2 }, Kata.Flip('R', new int[] { 2, 0, 1, 0 }));
+            Assert.AreEqual(new int[] { 0, 0, 1, 2 }, Kata.Flip('R', new int[] { 2, 0, 0, 1 }));
+
+
+        }
+
+
+        [Test]
+        public void Test_shift_left()
+        {
+            Assert.AreEqual(new int[] { 2, 1, 0, 0 }, Kata.Flip('L', new int[] { 0, 0, 2, 1 }));
+            Assert.AreEqual(new int[] { 2, 1, 0, 0 }, Kata.Flip('L', new int[] { 0, 2, 1, 0 }));
+            Assert.AreEqual(new int[] { 2, 1, 0, 0 }, Kata.Flip('L', new int[] { 2, 0, 1, 0 }));
+            Assert.AreEqual(new int[] { 2, 1, 0, 0 }, Kata.Flip('L', new int[] { 2, 0, 0, 1 }));
+
+
+        }
+
+        [Test]
+        public void Test_shift_left2()
+        {
+            Assert.AreEqual(new int[] { 2, 1, 0, 0 }, Kata.Flip('L', new int[] { 0, 0, 2, 1 }));
+            Assert.AreEqual(new int[] { 2, 1, 0, 0 }, Kata.Flip('L', new int[] { 0, 2, 1, 0 }));
+            Assert.AreEqual(new int[] { 2, 1, 0, 0 }, Kata.Flip('L', new int[] { 2, 0, 1, 0 }));
+            Assert.AreEqual(new int[] { 2, 1, 0, 0 }, Kata.Flip('L', new int[] { 2, 0, 0, 1 }));
+
+
+        }
+    }
+}
